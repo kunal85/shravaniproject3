864bc1a1f992cdfb2ce7a6f620128a5adb6323f4
kunal85 <44514934+kunal85@users.noreply.github.com>
2018-10-28 16:51:23 -0600

    Add files via upload

---
+++ gugundabattulaProject3A/gugundabattulaProject3A/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gugundabattulaProject3A
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class startwithsix : Exception
+        {
+
+
+            public startwithsix()
+            {
+                MessageBox.Show("An exception has occured the number cannot start with 6 for the first number");
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string a = maskedTextBox1.Text;
+                decimal Number1 = Convert.ToDecimal(maskedTextBox1.Text);
+                decimal Number2 = Convert.ToDecimal(maskedTextBox2.Text);
+                decimal number3 = Number1 / Number2;
+                if (Number1 > 5000)
+                {
+                    throw new OverflowException();
+                };
+                if (a.StartsWith("6"))
+                {
+                    throw new startwithsix();
+                }
+                maskedTextBox3.Text = number3.ToString();
+                maskedTextBox1.Focus();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Format Exception has occured enter values in numbers only");
+                maskedTextBox1.Focus();
+            }
+            catch (OverflowException)
+            {
+                MessageBox.Show("Overflow exception has occured enter First Number less then 5000");
+                maskedTextBox1.Focus();
+            }
+            catch (DivideByZeroException)
+            {
+                MessageBox.Show("Divide by zero exception has occured enter a number greater than zero for Second Number");
+                maskedTextBox2.Focus();
+            }
+            catch (startwithsix)
+            {
+                maskedTextBox1.Focus();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ gugundabattulaProject3A/gugundabattulaProject3A/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("gugundabattulaProject3A")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("gugundabattulaProject3A")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("de018e28-1a91-450b-b32a-f70559e0b72e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
